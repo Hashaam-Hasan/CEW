ab2442d0500c018812a1c441ffe2b96e2f3e60ad
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-10-24 23:50:24 +0500

    Update Task-3.c

--- LAB 1/Task-3.c
+++ LAB 1/Task-3.c
@@ -16,6 +16,9 @@ int main (){
     if (num > 0){
         tableprint(num);    //calling the function
     }
+    else if (num <= 0){
+        printf("Invalid Input");
+    }
 
 }
 // Defining function for printing table
@@ -33,3 +36,36 @@ void tableprint(int num){
     printf("%d x 10 = %d\n",num,num*10);
     
 }
+
+
+//We can also use another statement instead of if else if statement
+
+
+#include <stdio.h>
+
+int main (){
+
+    int num;
+    
+    printf("Enter any integer: \n");
+    scanf("%d", &num);
+    (num < 0)? printf("Invalid Input\n") : tableprint(num);
+
+}
+// Defining function for printing table
+int tableprint(x){
+    
+    printf("%d x 1 = %d\n",x,x*1);
+    printf("%d x 2 = %d\n",x,x*2);
+    printf("%d x 3 = %d\n",x,x*3);
+    printf("%d x 4 = %d\n",x,x*4);
+    printf("%d x 5 = %d\n",x,x*5);
+    printf("%d x 6 = %d\n",x,x*6);
+    printf("%d x 7 = %d\n",x,x*7);
+    printf("%d x 8 = %d\n",x,x*8);
+    printf("%d x 9 = %d\n",x,x*9);
+    printf("%d x 10 = %d\n",x,x*10);
+    
+}
+
+
