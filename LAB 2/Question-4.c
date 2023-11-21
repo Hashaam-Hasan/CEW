4bf0b9a68226a2af6f7fb41444d438775c5f5b78
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-11-22 00:23:19 +0500

    Update Question-4.c

--- LAB 2/Question-4.c
+++ LAB 2/Question-4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char sentence[1000];
+    printf("Enter sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    for (int i = 0; sentence[i] != '\0'; i++) {
+        if (isalpha(sentence[i])) {
+            if (islower(sentence[i])) {
+                sentence[i] = toupper(sentence[i]);
+            } else {
+                sentence[i] = tolower(sentence[i]);
+            }
+        }
+    }
+
+    printf("Replaced sentence: %s\n", sentence);
+
+    return 0;
+}
 
