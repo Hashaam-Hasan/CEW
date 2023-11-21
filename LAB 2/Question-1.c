267a3ae810633e9c88dcde5f3302af32044eefda
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-11-22 00:19:34 +0500

    Update Question-1.c

--- LAB 2/Question-1.c
+++ LAB 2/Question-1.c
@@ -1,1 +1,59 @@
+//FOR LOOP:
+
+#include <stdio.h>
+
+int main() {
+    int num , tot_sum = 0;
+    printf("Enter any number:");
+    scanf("%d",&num);
+    for (int i = 1, count = 0; count < num; i++) {
+        if (i % 2 != 0) {
+            printf("%d ", i);
+            tot_sum += i;
+            count++;
+        }
+    }
+    printf("\nSum: %d\n", tot_sum);
+}
+
+//WHILE LOOP:
+
+#include <stdio.h>
+
+int main() {
+    int num,i =1, tot_sum = 0, count = 0;
+    printf("Enter any Number:");
+    scanf("%d",&num);
+    while (count < num) {
+        if (i % 2 != 0) {
+            printf("%d ", i);
+            tot_sum += i;
+            count++;
+        }
+        i++;
+    }
+    printf("\nSum: %d\n", tot_sum);
+}
+
+
+//DO-WHILE LOOP:
+
+#include <stdio.h>
+
+int main() {
+    int num, tot_sum = 0, i = 1, count = 0;
+    printf("Enter any number:");
+    scanf("%d",&num);
+    do {
+        if (i % 2 != 0) {
+            printf("%d ", i);
+            tot_sum += i;
+            count++;
+        }
+        i++;
+    } while (count < num);
+    printf("\nSum: %d\n", tot_sum);
+
+    return 0;
+}
 
