08b28fc6b471d058e21c98534672e771f1e7591a
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-11-22 00:21:07 +0500

    Update Question-2.c

--- LAB 2/Question-2.c
+++ LAB 2/Question-2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i=0;
+    printf("Enter rows:");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++) {
+        for (int j = rows-i;j>0;j--) {
+            printf(" ");
+        }
+        for (int k= 1; k<= i; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
