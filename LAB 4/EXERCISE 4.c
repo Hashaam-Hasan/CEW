7117af3f6a20d56bfee3d57504d4bb13a69881b3
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-11-22 01:01:47 +0500

    EXERCISE 4.c

---
+++ LAB 4/EXERCISE 4.c
@@ -0,0 +1,14 @@
+//Function to Remove elements form odd indices of a singly linked list
+struct Node* RemoveOdd(struct Node* head){
+    struct Node*current=head;
+    struct Node*nodetodlt=current->next;
+    while(current!=NULL && nodetodlt!=NULL){
+        current->next=nodetodlt->next;
+        free(nodetodlt);
+        current=current->next;
+        if(current!=NULL){
+        nodetodlt=current->next;
+        }
+    }
+    return head;
+};
