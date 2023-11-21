70d4424c74c5be69f5868807f8541f5ecc79d1c0
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2023-11-22 00:24:18 +0500

    Update Question-6.c

--- LAB 2/Question-6.c
+++ LAB 2/Question-6.c
@@ -1,1 +1,29 @@
+# include <stdio.h>
+
+struct Distance {
+    int feet;
+    int inches;
+}
+int main() {
+    struct Distance distance1, distance2, sum;
+    printf("Enter 1st distance:\n");
+    printf("Feet: ");
+    scanf("%d", &distance1.feet);
+    printf("Inches: ");
+    scanf("%d", &distance1.inches);
+    printf("Enter 2nd distance:\n");
+    printf("Feet: ");
+    scanf("%d", &distance2.feet);
+    printf("Inches: ");
+    scanf("%d", &distance2.inches);
+    sum = addDistances(distance1, distance2);
+    printf("Sum of distances: %d feet %d inches \n", sum.feet, sum.inches);
+    return 0;
+}
+struct Distance addDistances(struct Distance d1, struct Distance d2) {
+    struct Distance result;
+    result.feet = d1.feet + d2.feet;
+    result.inches = d1.inches + d2.inches;
+    return result;
+}
 
